9323700a09b98eda873bea7de15abde18fe8e7d8
Harabe <harabex1321@gmail.com>
2024-04-22 18:02:10 +0000

    Added chart.js

--- ImgUpload/src/App.svelte
+++ ImgUpload/src/App.svelte
@@ -3,14 +3,17 @@
   import Navbar from './lib/Navbar/Navbar.svelte'
   import TestComponent from './lib/Components/TestComponent.svelte';
   import getIconStore from './js/IconStore';
-
+  import Dashboard from './lib/Pages/Dashboard.svelte';
   const icons = getIconStore();
 
 </script>
 
+<div class="flex flex-col">
   <Navbar></Navbar>
-  
-  <TestComponent> </TestComponent>  
+  <div  class="w-full h-screen bg-base-300">
+     <Dashboard></Dashboard>
+  </div>
+</div>
 <style>
   
 </style>
---
+++ ImgUpload/src/lib/Pages/Dashboard.svelte
@@ -0,0 +1,23 @@
+<script>   
+   import StatsFrame from "../DataPresenters/Stats/StatsFrame.svelte";    
+    import Stat from "../DataPresenters/Stats/Stat.svelte";
+</script>
+ 
+<StatsFrame>
+    <Stat title="Revanue" centered={true} value="23,300$"         description="Lorem ipsum dolor sit amet">
+    </Stat>
+
+    <Stat title="Revanue" color="text-secondary" centered={true} value="23,300$"         description="Lorem ipsum dolor sit amet">
+    </Stat>
+
+    <Stat title="Revanue" centered={true} value="23,300$"         description="Lorem ipsum dolor sit amet">
+    </Stat>
+    
+</StatsFrame>
+
+
+
+
+
+
+
