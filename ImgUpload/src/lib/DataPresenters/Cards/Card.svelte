9e44a1aacba2639f51bd986267d668474d2b50cd
Harabe-x <harabex1321@gmail.com>
2024-05-02 21:40:06 +0200

    Added button to api key card

--- ImgUpload/src/lib/DataPresenters/Cards/Card.svelte
+++ ImgUpload/src/lib/DataPresenters/Cards/Card.svelte
@@ -6,12 +6,16 @@
 
 </script>
 
-<div class="card w-full p-6 bg-base-100 shadow-xl mt-6 {containerStyle}" >
-
-      <CardSubTitle>
-        {title}        
-      </CardSubTitle>
+<div class="card  w-full p-6 bg-base-100 shadow-xl mt-6 {containerStyle}" >
+    
+    <div class="flex flex-row w-full items-center">
+        <CardSubTitle>
+          {title}        
+        </CardSubTitle>
       
+        <slot name="titleControl" />
+    </div>
+
       <div class="divider mt-2"></div>
   
       <div class='h-full w-full pb-6 bg-base-100'>
--- ImgUpload/src/lib/Pages/Api/ApiPage.svelte
+++ ImgUpload/src/lib/Pages/Api/ApiPage.svelte
@@ -2,12 +2,22 @@
     import Card from '../../DataPresenters/Cards/Card.svelte'
     import Chart from '../../DataPresenters/Charts/Chart.svelte';
     import { getChartData } from '../../../js/ApplicationData/ChartData';
+    import { Icon,Plus } from 'svelte-hero-icons';
 </script>
     
 
 <div class="grid mt-1 grid-cols-1">
      
-    <Card title="Api Keys"> </Card>
+    <Card title="Api Keys">
+        <!-- Title Button  -->
+            <div class="flex flex-row ml-auto mr-2" slot="titleControl"> 
+                <button class="btn btn-ghost btn-sm normal-case">
+                     <Icon src={Plus} class="w-5 h-5 "></Icon>
+                     Add
+                </button> 
+            </div>
+             
+    </Card>
 </div> 
  
 <div class="grid lg:grid-cols-2 mt-2 grid-cols-1 gap-10">
