0126866dcd2f9be6dad19477324f99e89dabec23
Harabe-x <harabex1321@gmail.com>
2024-05-18 23:06:01 +0200

    changed ImageBrowser

--- ImgUpload/src/lib/DataPresenters/Cards/Stat.svelte
+++ ImgUpload/src/lib/DataPresenters/Cards/Stat.svelte
@@ -13,14 +13,14 @@
 
   
 <div class="stats shadow">
-    <div class="stat">
+    <div class="stat bg-base-300">
         <div class="stat-figure text-primary"> 
             <Icon src={icon}  class="w-20 h-20 text-{iconColor}" ></Icon>
          </div>
         <div class="stat-title font-semibold">
             <span class="text-{titleTextColor}"> {title} </span>
         </div>
-        <div class="stat-value">
+        <div class="stat-value text-s">
             <span class="text-{valueTextColor}"> {value} </span>
         </div>
         <div class="stat-desc">{description}</div>
--- ImgUpload/src/lib/Pages/Images/Components/ImageBrowserModal.svelte
+++ ImgUpload/src/lib/Pages/Images/Components/ImageBrowserModal.svelte
@@ -1,13 +1,12 @@
 <script> 
     import { createEventDispatcher } from 'svelte'
     import IconButton from "../../../Controls/Buttons/IconButton.svelte";
-    import {ArrowDownTray, ArrowUpOnSquare, XMark} from "svelte-hero-icons";
+    import {ArrowDownTray, ArrowUpOnSquare, ChevronLeft, ChevronRight, Icon, XMark} from "svelte-hero-icons";
+    import Stat from '../../../DataPresenters/Cards/Stat.svelte';
 
     export let isModalVisable = false; 
     export let param; 
-    export let title = null; 
-    export let description = null; 
-    
+
     const dipsatcher = createEventDispatcher()
 
     function closeModal()
@@ -22,7 +21,7 @@
         <div class="w-3/4 h-3/4 bg-base-300 rounded-xl">
 
         <!-- Top Panel  -->
-            <div class="flex flex-row w-full h-[10%]  ">
+            <div class="flex flex-row w-full h-[15%]  ">
                 <div class="flex w-1/3  items-center ml-3 " >
                     <IconButton on:click={closeModal} icon={XMark} iconStyle="w-5 "></IconButton>
                 </div>
@@ -34,12 +33,24 @@
                     <IconButton icon={ArrowDownTray} iconStyle="w-5 sm:3"  flipIcons={true} buttonStyle="lg:bg-success lg:text-primary-content  ">Download</IconButton>
                 </div>
               </div>
-            <div class="h-[80%] w-full flex flex-row">
+            <div class="h-[70%] w-full flex  flex-row">
+                <div class="h-full w-[15%] flex flex-col items-center justify-center">
+                    <IconButton icon={ChevronLeft} iconStyle="w-8"></IconButton>
+                </div>
+
+                <div class="h-full w-[70%] flex justify-center items-center">
+                        <img src={param} alt="Requested" class="max-w-[80%] rounded-md">
+                </div>
+
+
+                <div class="h-full w-[15%] flex flex-col items-center justify-center">
+                    <IconButton icon={ChevronRight} iconStyle="w-8"></IconButton>
+                </div>
 
             </div>
 
         <!-- Info Section -->
-            <div class="h-[10%] flex flex-row">
+            <div class="h-[15%] flex flex-row">
 
             </div>
 
