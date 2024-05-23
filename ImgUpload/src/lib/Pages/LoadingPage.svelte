86e81c92dc7de8cf0ef7861a4a6de312ec9d99aa
Harabe <harabex1321@gmail.com>
2024-05-23 13:02:19 +0200

    Update LoadingPage.svelte

--- ImgUpload/src/lib/Pages/LoadingPage.svelte
+++ ImgUpload/src/lib/Pages/LoadingPage.svelte
@@ -5,4 +5,4 @@
 <div class="w-full h-screen text-gray-300 dark:text-gray-200 bg-base-200">
         Loading content ... 
 </div>
-    
+    https://sturdy-xylophone-jpxwpg65j44hqp6g-5173.app.github.dev
