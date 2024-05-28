9f56f0e433ea9f79180b216f0e0c50de3cdc6dce
Harabe-x <harabex1321@gmail.com>
2024-05-28 17:33:27 +0200

    Changed CollectionBrowser to work with ImageBrowserStore

--- ImgUpload/src/lib/Pages/Images/CollectionBrowser.svelte
+++ ImgUpload/src/lib/Pages/Images/CollectionBrowser.svelte
@@ -1,49 +1,49 @@
   <script>
     import {ArrowLeft, ArrowPath, Icon, Plus} from "svelte-hero-icons";
-    import { getPhotoList } from "../../../js/Temp/PhotoPlaceholderApi.js";
     import IconButton from "../../Controls/Buttons/IconButton.svelte";
     import PageTopMenu from "../../Controls/Shared/PageTopMenu.svelte";
-    import DataFetchingInfo from "../../Controls/Shared/DataFetchingInfo.svelte";
     import ImageFrame from "./Components/ImageFrame.svelte";
     import ModalWindow from "../../Controls/Shared/ModalWindow.svelte";
     import DataPaginator from "../../Controls/Shared/DataPaginator.svelte";
     import Card from "../../Controls/Cards/Card.svelte";
     import {onMount} from "svelte";
-    import PageHeaderNameStore from "../../../js/Temp/PageHeaderNameStore.js";
     import pageHeaderNameStore from "../../../js/Temp/PageHeaderNameStore.js";
     import {getSelectedCollectionDataStore} from "../../../js/Temp/SelectedCollectionStore.js";
     import ImagesPage from "./ImagesPage.svelte";
     import {getNavigationStore} from "../../../js/Temp/NavigationStore.js";
+    import createImageBrowserStore from "../../../js/ImageBrowserStore.js";
+    import {get} from "svelte/store";
 
     const selectedPageStore = getSelectedCollectionDataStore();
     const pageHeaderStore = pageHeaderNameStore()
     const navigationStore = getNavigationStore()
+    const imageBrowserStore = createImageBrowserStore();
     const imagesPerPage = 32;
 
-    let promise =   getPhotoList(1,imagesPerPage);;
     let imageModalToggleFunction;
     let addImageModalToggleFunction;
-    let selectedImage;
-
+    let nextPageFunction;
+    let previousPageFunction;
 
     onMount(() => {
-        $pageHeaderStore = $selectedPageStore.CollectionName + ' Collection';
+        pageHeaderStore.update(_ => {
+            return get(selectedPageStore).CollectionName + ' Collection';
+        })
+
+        imageBrowserStore.setNextPageCallback(nextPageFunction)
+        imageBrowserStore.setPreviousPageCallback(previousPageFunction)
+        imageBrowserStore.fetchImages(imagesPerPage)
     })
-
-    function goBack()
-    {
-        $pageHeaderStore = "Images"
-        $navigationStore = ImagesPage;
-    }
-
-    function selectImage(image)
-    {
-        selectedImage = image;
-    }
 
     function changePage(event)
     {
-       promise = getPhotoList(event.detail,imagesPerPage)
+        imageBrowserStore.goToSelectedPage(event.detail)
+    }
+
+    function goBack()
+    {
+        pageHeaderStore.update(_ => { return "Images"})
+        navigationStore.update(_ => { return ImagesPage });
     }
 
 </script>
@@ -63,26 +63,19 @@
         <div  slot="titleControl" class="ml-auto">
             <IconButton icon={Plus} iconStyle="w-4" on:click={addImageModalToggleFunction} > Add Image  </IconButton>
         </div>
-            {#await promise }
-                <DataFetchingInfo></DataFetchingInfo>
-            {:then result }
+            <div class="grid lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-1 gap-10">
+            {#each  $imageBrowserStore.images as image (image)}
+                <button on:click={() => { imageBrowserStore.selectImage(image);  imageModalToggleFunction() }}>
+                    <ImageFrame imgSrc={image}></ImageFrame>
+                </button>
+            {/each}
+        </div>
 
-                <div class="grid lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-1 gap-10">
-
-                {#each result as item }
-                    <button on:click={() => { selectImage(item.download_url); imageModalToggleFunction(); }}>
-                        <ImageFrame imgSrc={item.download_url}></ImageFrame>
-                    </button>
-                {/each}
-                </div>
-            {:catch error}
-                Something went wrong
-            {/await}
         <div class="flex flex-row items-center justify-center mr-4  mt-5">
-            <DataPaginator on:navigatedToNextPage={changePage} on:navigatedToPreviousPage={changePage} ></DataPaginator>
+            <DataPaginator on:navigatedToNextPage={changePage} on:navigatedToPreviousPage={changePage} bind:nextPage={nextPageFunction} bind:previousPage={previousPageFunction}></DataPaginator>
         </div>
     </Card>
 </div>
 
 <ModalWindow type="AddImageModal" bind:toggleModal={addImageModalToggleFunction} ></ModalWindow>
-<ModalWindow type="ImageBrowserModal" bind:toggleModal={imageModalToggleFunction} param={selectedImage}> </ModalWindow>
+<ModalWindow type="ImageBrowserModal" param={imageBrowserStore} bind:toggleModal={imageModalToggleFunction}> </ModalWindow>
